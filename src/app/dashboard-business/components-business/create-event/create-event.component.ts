d6a40b9736542abf515bf86be1282d0911e68f1c
claraecheverria <agustin.echeverria@fing.edu.uy>
2023-10-18 09:00:40 -0300

    add form controls to create event

--- src/app/dashboard-business/components-business/create-event/create-event.component.ts
+++ src/app/dashboard-business/components-business/create-event/create-event.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-event.component.css']
 })
 export class CreateEventComponent {
+  eventForm: FormGroup;
 
+  constructor() {
+    this.eventForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      location: new FormControl('', Validators.required),
+      neighborhood: new FormControl('', Validators.required),
+      date: new FormControl('', Validators.required),
+      time: new FormControl('', Validators.required),
+      genre: new FormControl(''),
+      equipment: new FormControl(''),
+      payment: new FormControl(false),
+      description: new FormControl(''),
+    });
+  }
+
+onSubmit() {
+  console.log(this.eventForm.value);
+  // You can send this.eventForm.value to your backend API
+}
 }
