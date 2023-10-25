53dcba4c66dd082c0983c61a643e88a262619e84
claraecheverria <agustin.echeverria@fing.edu.uy>
2023-10-24 22:12:51 -0300

    expand artist profile from event general

--- src/app/dashboard/components/artist-profile-event/artist-profile-event.component.ts
+++ src/app/dashboard/components/artist-profile-event/artist-profile-event.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { UserArtistService } from 'src/app/services/user/user-artist.service';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
   templateUrl: './artist-profile-event.component.html',
   styleUrls: ['./artist-profile-event.component.css']
 })
-export class ArtistProfileEventComponent {
+export class ArtistProfileEventComponent implements OnInit{
   links: string[] = [ //lista de links
     'http://www.google.com',
     'http://www.example.com',
@@ -18,11 +19,43 @@ export class ArtistProfileEventComponent {
 
   instagramTag: string ="cristiano"; 
   instagramLink: string=`https://www.instagram.com/${this.instagramTag}/`;
+  artistId: number =0;
+  eventId: string ='';
+  artisticName: string='';
+  picture: string='';
+  musicGenre: string='';
+  description:string='';
 
-  constructor(private matDialog:MatDialog, private route: ActivatedRoute, private router: Router) { }
+  constructor(private matDialog:MatDialog, private route: ActivatedRoute, private router: Router, private userArtistService: UserArtistService) { }
 
-  goBackToExpandedEvent(eventId: number) {
-    this.router.navigate(['/dashboard/expanded-event', eventId],{ relativeTo: this.route });
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.artistId = +params['artistId']; // The '+' sign converts it to a number
+      this.eventId = params['eventId'];
+    });
+    this.fetchAritist(this.artistId);
+    
+  }
+
+  fetchAritist(id: number): void {
+    this.userArtistService.getArtisytById(id).subscribe(
+      (artistData) => {
+        this.artisticName = artistData.artisticName;
+        this.picture= artistData.picture;
+        this.musicGenre=artistData.musicGenre.charAt(0).toUpperCase() + artistData.musicGenre.slice(1).toLowerCase();//Transforms it so that the first letter is upper case
+        this.description=artistData.description;
+        this.instagramTag=artistData.igUsername;
+        this.links=artistData.links;
+
+        
+      },
+      (error) => {
+        console.error('Error fetching business:', error);
+      }
+    );
+  }
+  goBackToExpandedEvent() {
+    this.router.navigate(['/dashboard/expanded-event', this.eventId],{ relativeTo: this.route });
   }
 
 
